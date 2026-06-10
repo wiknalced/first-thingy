48e632fa984cde729e7cff174264f09a66ad6ae7
wiknalced <dkiw@students.mags.school.nz>
2026-06-10 12:34:44 +1200

    Finalized map layout and character movement

---
+++ project-thing/Scripts/enemy.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+var speed:float = 250.0
+var player: CharacterBody2D
+var health : int = 10
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for node in get_tree().get_nodes_in_group("player"):
+		player = node
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if not player == null:
+		look_at(player.global_position)
+		velocity = Vector2(1,0).rotated(rotation) * speed
+		move_and_slide()
--- project-thing/Scripts/player.gd
+++ project-thing/Scripts/player.gd
@@ -2,16 +2,10 @@ extends CharacterBody2D
 
 
 var speed : float = 300
-const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
 
-	# Handle jump.
-	
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction : Vector2 = Vector2(0.0, 0.0)
 	direction.x = Input.get_axis("ui_left", "ui_right")
 	direction.y = Input.get_axis("ui_up", "ui_down")
